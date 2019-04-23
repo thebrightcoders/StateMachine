3d14bfd20ed46df8a4fdb6380909b33e975dc808
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-04-23 13:56:23 +0430

    New Update

---
+++ StateMachine/StateMachineTests/StateMachineTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StateMachinePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachinePack.Tests
+{
+    [TestClass()]
+    public class StateMachineTests
+    {
+        [TestMethod()]
+        public void AddLayer2paramsDefaultLayerTest()
+        {
+
+            StateMachine stateMachine = new StateMachine();
+            try
+            {
+                stateMachine.AddLayer("DEFAULT");
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void AddLayer2paramsDefaultWithSpacesLayerTest()
+        {
+            StateMachine stateMachine = new StateMachine();
+            try
+            {
+                Assert.IsNull(stateMachine.AddLayer("        DEFAULT       "));
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void AddLayer2paramsDefaultNullStatesWithSpacesLayerTest()
+        {
+            StateMachine stateMachine = new StateMachine();
+
+            try
+            {
+                Assert.IsNull(stateMachine.AddLayer("        DEFAULT       ", null));
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void AddLayer2paramsNullStatesLayerTest()
+        {
+            StateMachine stateMachine = new StateMachine();
+            try
+            {
+                Assert.IsNull(stateMachine.AddLayer("ALi", null));
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
