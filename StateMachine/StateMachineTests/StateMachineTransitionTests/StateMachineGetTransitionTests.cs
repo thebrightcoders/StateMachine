6da782ae2c3eee067b3ed9e947b62440ba7ac9ee
OmidRezaei <omidr1998@yahoo.com>
2019-05-03 11:41:09 +0430

    Transition Preparation
    Creating test files fro transition implementation

---
+++ StateMachine/StateMachineTests/StateMachineTransitionTests/StateMachineGetTransitionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StateMachineTests.StateMachineTransitionTests
+{
+    [TestClass]
+    public class StateMachineGetTransitionTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ StateMachine/StateMachineTests/StateMachineTransitionTests/StateMachineRemoveTransitionTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StateMachineTests.StateMachineTransitionTests
+{
+    [TestClass]
+    public class StateMachineRemoveTransitionTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
