464b01bcf45ea0944804fa0aaece60128f973d42
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-05-05 10:39:35 +0430

    New Update
    Some of StateMchineRemoveState Tests are implemented

---
+++ StateMachine/StateMachineTests/StateMachineStateTests/StateMachineRemoveStatesTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StateMachinePack;
+
+namespace StateMachineTests.StateMachineStateTests
+{
+    [TestClass]
+    public class StateMachineRemoveStatesTests
+    {
+        private StateMachine stateMachine;
+        [TestInitialize]
+        public void SetUp()
+        {
+            stateMachine = StateMachineBuilder.Build().WithLayers().WithStates();
+         
+            //---------------------------
+            stateMachine.AddLayer("LayerA");
+
+            stateMachine.AddState("e");
+            stateMachine.AddState("f");
+            stateMachine.AddState("g");
+            stateMachine.AddState("h");
+            stateMachine.AddState("i");
+            stateMachine.AddState("j");
+            //---------------------------
+            stateMachine.AddLayer("LayerB");
+            stateMachine.AddState("a15445");
+            stateMachine.AddState("b");
+            stateMachine.AddState("c");
+            stateMachine.AddState("d");
+            stateMachine.AddState("k");
+            stateMachine.AddState("l");
+            stateMachine.AddState("m");
+        }
+        [TestMethod]
+        public void StateMachineRemoveStateWithNullId()
+        {
+            Assert.ThrowsException<NullReferenceException>(() =>
+                stateMachine.RemoveState(null));
+        }
+        [TestMethod]
+        public void StateMachineRemoveStateDefaultStateFromDefaultLayer()
+        {
+            stateMachine.RemoveState(Layer.DEFAULTSTARTSTATEID, Layer.DEFAULT);
+            Assert.IsFalse(stateMachine.HasState(Layer.DEFAULTSTARTSTATEID,Layer.DEFAULT));
+        }
+        [TestMethod]
+        public void StateMachineRemoveStateAFromLayerA()
+        {
+            stateMachine.RemoveState("a15445", "LayerA");
+//            Assert.IsFalse(stateMachine.HasState(Layer.DEFAULTSTARTSTATEID, Layer.DEFAULT));
+// ASK OMID
+        }
+    }
+}
